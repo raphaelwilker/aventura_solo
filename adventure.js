898b00e2efdf3407bb9de54090644d61d2b4f3c9
Raphael Wilker Mendes Araujo <raphael.araujo@pearson.com>
2018-02-16 09:12:32 -0200

    (master) Continuacao da escrita do json

--- adventure.js
+++ adventure.js
@@ -25,4 +25,109 @@ var json  = [{
 		"text": "<li><a href='#'>Fugir ?</a></li>",
 		"goto": 6
 	}]
+},{
+	"text": "<p>Fugir seria tolice. Seis homens-lagartos são uma ameça séria para um aventureiro qualquer, é verdade... mas você não é um qualquer! Éum guerreiro de Hedryl, empunhando uma Espada Sagrada. Contra você esses asquerosos predadores do pântano são menos que NADA!</p><p>As abominações escamosas tombam uma a uma sob o ataque de sua lâmina. Tenso demais para dormir, você decide que o Pântano da Agonia não é seguro para um cochilo - e resolve seguir viagem.</p><p>Após ter se distanciado do Pântano da Agonia, você descansa um pouco. Ora aos deuses, afradecendo sua bondade por permitir que continue vivo - e, em resposta, os pouco ferimento que recebeu durante a luta curam-se como que por mágica.</p><p>Mágica dos deuses, você sabe.</p><p>Ao amanhecer você resolve comer algo e descansar um pouco antes de continuar a viagem. Afinal, sua armadura é de ferro, mas você não</p><p>A viagem continua calma, sem mais nenhum problema relevante, e permanece assim até o meio da tarde. Em certo ponto o caminho se bifurca.</p>",
+	"options": [{
+		"text": "<li><a href='#'>Ir para a direita.</a></li>",
+		"goto": 7
+	}, {
+		"text": "<li><a href='#'>Ir para a esqueda.</a></li>",
+		"goto": 8
+	}]
+},{
+	"text": "<p>Você se deixa dominar pelo medo e vira-se para fugir.<br>Antes que consiga dar dois passos, contudo, sente uma picada no ombro. Os homens-lagartos estão disparando dardos com zarabatanas, e um deles atingiu-o</p><p>Depois de uma curta corrida, você deixa seus lemntos perseguidores para trás - mas percebe que seu corpo esta ardendo em febre intensa. O dardo estava envenenado, e mesmo sua saúde excepcional não consegue anular a forte toxina que paralisa seu corpo. Ao final de uma hora, todos os seus músculos estão imóveis exceto o coração, que ainda pulsa com dificuldade quando os homens-lagartos finalmente aparecem.</p><p>É... você vai ser servido com batatas no jantar!</p><p>A viagem continua calma, sem mais nenhum problema relevante, e permanece assim até o meio da tarde. Em certo ponto o caminho se bifurca.</p>",
+	"options": [{
+		"text": "<li><a href='#'>Ir para a direita.</a></li>",
+		"goto": 7
+	}, {
+		"text": "<li><a href='#'>Ir para a esqueda.</a></li>",
+		"goto": 8
+	}]
+},{
+	"text": "<p>A jornada pelo caminho da direita é tranquila. Uma estalagem surge ao longe, e você apressa o passo - imaginando quão bom será comer uma refeição quente e dormir em uma cama macia. Você consegue um quarto, devora um grande pedaço de carne (felizmente sua ordem não exige dieta vegetariana), lava-se e vai dormir.</p><p>Pela manhã, após um rápido desjejum, você retoma o caminho da vila de Terum. A estrada birfuca-se novamente logo adiante: o caminho da direita segue sobre uma montanha, e o da esquerda passa por um vale que contorna a montanha.</p>",
+	"options": [{
+		"text": "<li><a href='#'>Seguir pela montanha ? 18</a></li>",
+		"goto": 9
+	}, {
+		"text": "<li><a href='#'>Seguir pelo vale ? 36</a></li>",
+		"goto": 10
+	}]
+},{
+	"text": "<p>O caminho da esquerda leva vocÊ a um gingantesco desfiladeiro. A fenda é enorme, e não há como chegar ao outro lado. Irritado pela perda de tempo, você volta para a encruzilhada e toma o caminho da direita</p>",
+	"options": [{
+		"text": "<li><a href='#'>Continuar.</a></li>",
+		"goto": 7
+	}]
+},{
+	"text": "<p>O caminho da montanha pareceu mais atraente. Seguindo por uma trilha que passa entre as rochas, sob sombras refrescantes, você pensa em como seria penosa a caminhada pelo vale castigado pelo sol. Mas a trilha rochosa também apresenta suas desvantagens, como você descobre logo a seguir: vinde de uma caverna próxima, um urro poderoso anuncia a chegada de um enorme urso cinzento.</p><p>A fera aproximan-se nas patas de trás, pronta a golpear  com suas garras negras. Você saca a Espada Sagrada, mas entende que o animal apenas tenta proteger sua toca - talvez seja até mesmo uma fêmea com filhotes -, e não deseja matá-lo. A este pensamento , a lâmina emite uma luz intensa que fere os olhos do urso, afugentando-o.</p><p>Agradecendo aos deuses por uma solução pacífica para seu dilema, você segue em frente.</p><p></p>",
+	"options": [{
+		"text": "<li><a href='#'>Lutar ?</a></li>",
+		"goto": 5
+	}, {
+		"text": "<li><a href='#'>Fugir ?</a></li>",
+		"goto": 6
+	}]
+},{
+	"text": "<p>Preocupado com as pessoas que possam vir a ser mortas pelo dragão, você entra no Pântano da Agonia para ganhar tempo. Caminha através de lama e águas estagnadas até o poente, parando apenas por um instante para acender uma tocha e beliscar um  pedaço de carne defumada.</p><p>Durante essa rápida pausa, ruídos incomuns chegam até você. Passos aproximan-se, sombras movem-se adiante. Da escuridão surgem duas criaturas bípedes, esverdeadas e tremendamente feias: são scrags, os trolls d´agua.</p>",
+	"options": [{
+		"text": "<li><a href='#'>Lutar ?</a></li>",
+		"goto": 5
+	}, {
+		"text": "<li><a href='#'>Fugir ?</a></li>",
+		"goto": 6
+	}]
+},{
+	"text": "<p>Preocupado com as pessoas que possam vir a ser mortas pelo dragão, você entra no Pântano da Agonia para ganhar tempo. Caminha através de lama e águas estagnadas até o poente, parando apenas por um instante para acender uma tocha e beliscar um  pedaço de carne defumada.</p><p>Durante essa rápida pausa, ruídos incomuns chegam até você. Passos aproximan-se, sombras movem-se adiante. Da escuridão surgem duas criaturas bípedes, esverdeadas e tremendamente feias: são scrags, os trolls d´agua.</p>",
+	"options": [{
+		"text": "<li><a href='#'>Lutar ?</a></li>",
+		"goto": 5
+	}, {
+		"text": "<li><a href='#'>Fugir ?</a></li>",
+		"goto": 6
+	}]
+},{
+	"text": "<p>Preocupado com as pessoas que possam vir a ser mortas pelo dragão, você entra no Pântano da Agonia para ganhar tempo. Caminha através de lama e águas estagnadas até o poente, parando apenas por um instante para acender uma tocha e beliscar um  pedaço de carne defumada.</p><p>Durante essa rápida pausa, ruídos incomuns chegam até você. Passos aproximan-se, sombras movem-se adiante. Da escuridão surgem duas criaturas bípedes, esverdeadas e tremendamente feias: são scrags, os trolls d´agua.</p>",
+	"options": [{
+		"text": "<li><a href='#'>Lutar ?</a></li>",
+		"goto": 5
+	}, {
+		"text": "<li><a href='#'>Fugir ?</a></li>",
+		"goto": 6
+	}]
+},{
+	"text": "<p>Preocupado com as pessoas que possam vir a ser mortas pelo dragão, você entra no Pântano da Agonia para ganhar tempo. Caminha através de lama e águas estagnadas até o poente, parando apenas por um instante para acender uma tocha e beliscar um  pedaço de carne defumada.</p><p>Durante essa rápida pausa, ruídos incomuns chegam até você. Passos aproximan-se, sombras movem-se adiante. Da escuridão surgem duas criaturas bípedes, esverdeadas e tremendamente feias: são scrags, os trolls d´agua.</p>",
+	"options": [{
+		"text": "<li><a href='#'>Lutar ?</a></li>",
+		"goto": 5
+	}, {
+		"text": "<li><a href='#'>Fugir ?</a></li>",
+		"goto": 6
+	}]
+},{
+	"text": "<p>Preocupado com as pessoas que possam vir a ser mortas pelo dragão, você entra no Pântano da Agonia para ganhar tempo. Caminha através de lama e águas estagnadas até o poente, parando apenas por um instante para acender uma tocha e beliscar um  pedaço de carne defumada.</p><p>Durante essa rápida pausa, ruídos incomuns chegam até você. Passos aproximan-se, sombras movem-se adiante. Da escuridão surgem duas criaturas bípedes, esverdeadas e tremendamente feias: são scrags, os trolls d´agua.</p>",
+	"options": [{
+		"text": "<li><a href='#'>Lutar ?</a></li>",
+		"goto": 5
+	}, {
+		"text": "<li><a href='#'>Fugir ?</a></li>",
+		"goto": 6
+	}]
+},{
+	"text": "<p>Preocupado com as pessoas que possam vir a ser mortas pelo dragão, você entra no Pântano da Agonia para ganhar tempo. Caminha através de lama e águas estagnadas até o poente, parando apenas por um instante para acender uma tocha e beliscar um  pedaço de carne defumada.</p><p>Durante essa rápida pausa, ruídos incomuns chegam até você. Passos aproximan-se, sombras movem-se adiante. Da escuridão surgem duas criaturas bípedes, esverdeadas e tremendamente feias: são scrags, os trolls d´agua.</p>",
+	"options": [{
+		"text": "<li><a href='#'>Lutar ?</a></li>",
+		"goto": 5
+	}, {
+		"text": "<li><a href='#'>Fugir ?</a></li>",
+		"goto": 6
+	}]
+},{
+	"text": "<p>Preocupado com as pessoas que possam vir a ser mortas pelo dragão, você entra no Pântano da Agonia para ganhar tempo. Caminha através de lama e águas estagnadas até o poente, parando apenas por um instante para acender uma tocha e beliscar um  pedaço de carne defumada.</p><p>Durante essa rápida pausa, ruídos incomuns chegam até você. Passos aproximan-se, sombras movem-se adiante. Da escuridão surgem duas criaturas bípedes, esverdeadas e tremendamente feias: são scrags, os trolls d´agua.</p>",
+	"options": [{
+		"text": "<li><a href='#'>Lutar ?</a></li>",
+		"goto": 5
+	}, {
+		"text": "<li><a href='#'>Fugir ?</a></li>",
+		"goto": 6
+	}]
 }]
